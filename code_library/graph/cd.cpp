ea3038f86d6031d15c7e89db89927fbe1f66d3d9
MysteriousBits <si.islam214@gmail.com>
2026-05-01 19:16:32 +0600

    Replaced vector<set> with vector<vector> for tree in CD

--- code_library/graph/cd.cpp
+++ code_library/graph/cd.cpp
@@ -1,37 +1,34 @@
 
 struct CD {
-  vector<set<int>> tree;
+  vector<vector<int>>& tree;
   vector<int> par, sz;
-  CD(vector<vector<int>>& tree) {
-    for (auto& v : tree) {
-      this->tree.emplace_back(v.begin(), v.end());
-    }
+  vector<bool> dead;
+  CD(vector<vector<int>>& tree) : tree(tree) {
     par.resize(tree.size());
     sz.resize(tree.size());
+    dead.resize(tree.size());
     build(1, 0);
   }
   void build(int u, int p) {
     dfs(u, p);
-    int centroid = dfs(u, p, sz[u]);
-    par[centroid] = p;
-    vector<int> adj(tree[centroid].begin(), tree[centroid].end());
-    for (int v : adj) {
-      tree[v].erase(centroid);
-      tree[centroid].erase(v);
-      build(v, centroid);
+    u = dfs(u, p, sz[u]);
+    dead[u] = true;
+    par[u] = p;
+    for (int v : tree[u]) {
+      if (!dead[v]) build(v, u);
     }
   }
   void dfs(int u, int p) {
     sz[u] = 1;
     for (int v : tree[u]) {
-      if (v == p) continue;
+      if (v == p || dead[v]) continue;
       dfs(v, u);
       sz[u] += sz[v];
     }
   }
   int dfs(int u, int p, int n) {
     for (int v : tree[u]) {
-      if (v == p) continue;
+      if (v == p || dead[v]) continue;
       if (sz[v] > n / 2) return dfs(v, u, n);
     }
     return u;
