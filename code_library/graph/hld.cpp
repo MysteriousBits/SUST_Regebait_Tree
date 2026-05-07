cbf78c28ddddef3ded48795417f7e116b2f2b86d
MysteriousBits <si.islam214@gmail.com>
2026-05-07 23:42:14 +0600

    Simpler hld

--- code_library/graph/hld.cpp
+++ code_library/graph/hld.cpp
@@ -1,6 +1,6 @@
-VI heavy, chain, label, par, sz, dep;
+vector<int> heavy, chain, label, par, sz, dep;
 int timer;
-void dfsz(int u, int p, VVI& tree) {
+void dfsz(int u, int p, vector<vector<int>>& tree) {
   sz[u] = 1;
   par[u] = p;
   dep[u] = dep[p] + 1;
@@ -12,7 +12,7 @@ void dfsz(int u, int p, VVI& tree) {
     if (sz[v] > mx) mx = sz[v], heavy[u] = v;
   }
 }
-void dfshld(int u, int p, VVI& tree, int head) {
+void dfshld(int u, int p, vector<vector<int>>& tree, int head) {
   label[u] = timer++;
   chain[u] = head;
   if (heavy[u] != -1) dfshld(heavy[u], u, tree, head);
@@ -21,25 +21,27 @@ void dfshld(int u, int p, VVI& tree, int head) {
     dfshld(v, u, tree, v);
   }
 }
-void inithld(int n, VVI& tree) {
-  heavy = VI(n + 1, -1);
-  chain = VI(n + 1);
-  label = VI(n + 1);
-  sz = VI(n + 1);
-  par = VI(n + 1);
-  dep = VI(n + 1);
+void inithld(vector<vector<int>>& tree) {
+  int n = tree.size();
+  heavy = vector<int>(n, -1);
+  chain = vector<int>(n);
+  label = vector<int>(n);
+  sz = vector<int>(n);
+  par = vector<int>(n);
+  dep = vector<int>(n);
   timer = 1;
   dfsz(1, 0, tree);
   dfshld(1, 0, tree, 1);
 }
-ll query(int u, int v, fenwick& bit) {
+ll query(int u, int v /*, fenwick/segtree/anything& ds*/) {
   ll ret = 0;  // init value
   for (; chain[u] != chain[v]; v = par[chain[v]]) {
     if (dep[chain[u]] > dep[chain[v]]) swap(u, v);
-    // ret ?= bit.rsq(label[chain[v]], label[v]);
+    // ret ?= ds.query(label[chain[v]], label[v]);
   }
   if (dep[u] > dep[v]) swap(u, v);
-  // lca = u
-  // ret ?= bit.rsq(label[u], label[v]);
+  // lca is u
+  // ret ?= ds.query(label[u], label[v]);
   return ret;
 }
+// Updating u : ds.update(label[u]...)
