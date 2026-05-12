3d0df5c58170de5e30b89284c7d1dcb9c1bdf008
NAHDI51 <hossainnahdi8051@gmail.com>
2026-05-12 08:31:05 +0600

    Fixed a bug and removed the macros

--- code_library/numerical/fft.cpp
+++ code_library/numerical/fft.cpp
@@ -1,37 +1,44 @@
-typedef complex<double> C;
-typedef vector<double> vd;
 void fft(vector<C>& a) {
-	int n = sz(a), L = 31 - __builtin_clz(n);
-	static vector<complex<long double>> R(2, 1);
-	static vector<C> rt(2, 1);  // (^ 10% faster if double)
-	for (static int k = 2; k < n; k *= 2) {
-		R.resize(n); rt.resize(n);
-		auto x = polar(1.0L, acos(-1.0L) / k);
-		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
-	}
-	vi rev(n);
-	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
-			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
-			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
-			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
-			a[i + j + k] = a[i + j] - z;
-			a[i + j] += z;
-		}
+    int n = a.size(), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<C> rt(2, 1);
+    static int kBuilt = 2;
+    for (; kBuilt < n; kBuilt *= 2) {
+        R.resize(2 * kBuilt);
+        rt.resize(2 * kBuilt);
+        auto x = polar(1.0L, acos(-1.0L) / kBuilt);
+        for (int i = kBuilt; i < 2 * kBuilt; i++)
+            rt[i] = R[i] = (i & 1) ? R[i / 2] * x : R[i / 2];
+    }
+
+    vector<int> rev(n);
+    for (int i = 0; i < n; i++) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    for (int i = 0; i < n; i++) if (i < rev[i]) swap(a[i], a[rev[i]]);
+
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k)
+            for (int j = 0; j < k; j++) {
+                // Hand-rolled complex multiply (~25% faster than rt[j+k]*a[i+j+k])
+                auto x = (double*)&rt[j + k];
+                auto y = (double*)&a[i + j + k];
+                C z(x[0] * y[0] - x[1] * y[1],
+                    x[0] * y[1] + x[1] * y[0]);
+                a[i + j + k] = a[i + j] - z;
+                a[i + j]    += z;
+            }
 }
+
 vd conv(const vd& a, const vd& b) {
-	if (a.empty() || b.empty()) return {};
-	vd res(sz(a) + sz(b) - 1);
-	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
-	vector<C> in(n), out(n);
-	copy(all(a), begin(in));
-	rep(i,0,sz(b)) in[i].imag(b[i]);
-	fft(in);
-	for (C& x : in) x *= x;
-	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
-	fft(out);
-	rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
-	return res;
+    if (a.empty() || b.empty()) return {};
+    vd res(a.size() + b.size() - 1);
+    int L = 32 - __builtin_clz((int)res.size()), n = 1 << L;
+    vector<C> in(n), out(n);
+    copy(a.begin(), a.end(), in.begin());
+    for (int i = 0; i < (int)b.size(); i++) in[i].imag(b[i]);
+    fft(in);
+    for (C& x : in) x *= x;
+    for (int i = 0; i < n; i++) out[i] = in[-i & (n - 1)] - conj(in[i]);
+    fft(out);
+    for (int i = 0; i < (int)res.size(); i++) res[i] = imag(out[i]) / (4 * n);
+    return res;
 }
