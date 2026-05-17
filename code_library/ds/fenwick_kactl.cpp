f92afeae11ad9ba1cc2ebc1d5c66c1e94f58c71a
MysteriousBits <si.islam214@gmail.com>
2026-05-18 02:47:59 +0600

    added range query and update to bit

--- code_library/ds/fenwick_kactl.cpp
+++ code_library/ds/fenwick_kactl.cpp
@@ -1,22 +1,28 @@
 struct FT {
-	vector<ll> s;
-	FT(int n) : s(n) {}
-	void update(int pos, ll dif) { // a[pos] += dif
-		for (; pos < s.size(); pos |= pos + 1) s[pos] += dif;
-	}
-	ll query(int pos) { // sum of values in [0, pos)
-		ll res = 0;
-		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
-		return res;
-	}
-	int lower_bound(ll sum) {// min pos st sum of [0, pos] >= sum
-		// Returns n if no sum is >= sum, or -1 if empty sum is.
-		if (sum <= 0) return -1;
-		int pos = 0;
-		for (int pw = 1 << 25; pw; pw >>= 1) {
-			if (pos + pw <= s.size() && s[pos + pw-1] < sum)
-				pos += pw, sum -= s[pos-1];
-		}
-		return pos;
-	}
+  vector<ll> s;
+  FT(int n) : s(n) {}
+  void update(int pos, ll dif) {  // a[pos] += dif
+    for (; pos < s.size(); pos |= pos + 1) s[pos] += dif;
+  }
+  ll query(int pos) {  // sum of values in [0, pos)
+    ll res = 0;
+    for (; pos > 0; pos &= pos - 1) res += s[pos - 1];
+    return res;
+  }
+  // Use either range update or range query (1 based index)
+  ll query(int l, inr r) { return query(r) - query(l - 1); }
+  void update(int l, int r, ll dif) {
+    update(l, diff);
+    if (r < s.size()) update(r + 1, -diff);
+  }
+  int lower_bound(ll sum) {  // min pos st sum of [0, pos] >= sum
+    // Returns n if no sum is >= sum, or -1 if empty sum is.
+    if (sum <= 0) return -1;
+    int pos = 0;
+    for (int pw = 1 << 25; pw; pw >>= 1) {
+      if (pos + pw <= s.size() && s[pos + pw - 1] < sum)
+        pos += pw, sum -= s[pos - 1];
+    }
+    return pos;
+  }
 };
