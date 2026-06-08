986cb33eb1324a2dcd4e541fcbf00a73b115f55b
NAHDI51 <hossainnahdi8051@gmail.com>
2026-06-09 01:57:56 +0600

    FWHT: Removed the macros

--- code_library/numerical/fwht.cpp
+++ code_library/numerical/fwht.cpp
@@ -1,17 +1,17 @@
-// Fast Subset Transform
-void FST(vi& a, bool inv) {
-	for (int n = sz(a), step = 1; step < n; step *= 2) {
-		for (int i = 0; i < n; i += 2 * step) rep(j,i,i+step) {
-			int &u = a[j], &v = a[j + step]; tie(u, v) =
-				inv ? pii(v - u, u) : pii(v, u + v); // AND
-				// inv ? pii(v, u - v) : pii(u + v, u); // OR /// include-line
-				// pii(u + v, u - v);                   // XOR /// include-line
-		}
-	}
-	// if (inv) for (int& x : a) x /= sz(a); // XOR only /// include-line
+void FST(vector<int>& a, bool inv) {
+    for (int n = a.size(), step = 1; step < n; step *= 2) {
+        for (int i = 0; i < n; i += 2 * step) 
+            for(int j = i; j < i+step; j++) {
+            int &u = a[j], &v = a[j + step]; tie(u, v) =
+                inv ? pair<int,int>(v - u, u) : pair<int,int>(v, u + v); // AND
+                // inv ? pair<int,int>(v, u - v) : pair<int,int>(u + v, u); // OR /// include-line
+                // pair<int,int>(u + v, u - v);                   // XOR /// include-line
+        }
+    }
+    // if (inv) for (int& x : a) x /= a.size(); // XOR only /// include-line
 }
-vi conv(vi a, vi b) {
-	FST(a, 0); FST(b, 0);
-	rep(i,0,sz(a)) a[i] *= b[i];
-	FST(a, 1); return a;
+vector<int> conv(vector<int> a, vector<int> b) {
+    FST(a, 0); FST(b, 0);
+    for(int i = 0; i < a.size(); i++) a[i] *= b[i];
+    FST(a, 1); return a;
 }
