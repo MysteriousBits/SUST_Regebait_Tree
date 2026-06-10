7bc62131d9ebb85c4a9e7efc7d9b4856b2500f9a
MysteriousBits <si.islam214@gmail.com>
2026-06-11 03:09:13 +0600

    Replaced with clean bcc, bridge, ap template

--- code_library/graph/bcc.cpp
+++ code_library/graph/bcc.cpp
@@ -1,45 +1,39 @@
 // g[u] = {{neighbor, edge_id}, ...}
 vector<vector<pair<int,int>>> g;
-vector<int> tin, low, st;
+vector<int> tin, st, bridges;
 vector<bool> is_art;
 vector<vector<int>> bccs;
 int timer;
-void dfs(int u, int pe) {
-  tin[u] = low[u] = ++timer;
-  int ch = 0;
-  for (auto [v, id] : g[u]) {
-    if (id == pe) continue;
+int dfs(int u, int pe) {
+  int me = tin[u] = ++timer, top = me, ch = 0;
+  for (auto [v, id] : g[u]) if (id != pe) {
     if (tin[v]) {
-      low[u] = min(low[u], tin[v]);
-      if (tin[v] < tin[u]) st.push_back(id);
+      top = min(top, tin[v]);
+      if (tin[v] < me) st.push_back(id);
     } else {
       ch++;
-      st.push_back(id);
-      dfs(v, id);
-      low[u] = min(low[u], low[v]);
-      if (low[v] >= tin[u]) {
+      int si = st.size();
+      int up = dfs(v, id);
+      top = min(top, up);
+      if (up >= me) {
         if (pe != -1) is_art[u] = true;
-        bccs.emplace_back();
-        do {
-          bccs.back().push_back(st.back());
-          st.pop_back();
-        } while (bccs.back().back() != id);
+        if (up == me) {
+          st.push_back(id);
+          bccs.push_back({st.begin() + si, st.end()});
+          st.resize(si);
+        } else {
+          bridges.push_back(id);
+        }
+      } else {
+        st.push_back(id);
       }
     }
   }
   if (pe == -1 && ch > 1) is_art[u] = true;
+  return top;
 }
 void get_bcc(int n) {
-  tin.assign(n + 1, 0); low.assign(n + 1, 0);
-  is_art.assign(n + 1, false);
-  st.clear(); bccs.clear(); timer = 0;
-  for (int i = 1; i <= n; i++) {
-    if (!tin[i]) {
-      dfs(i, -1);
-      if (!st.empty()) {
-        bccs.push_back(st);
-        st.clear();
-      }
-    }
-  }
+  tin.assign(n + 1, 0); is_art.assign(n + 1, false);
+  st.clear(); bccs.clear(); bridges.clear(); timer = 0;
+  for (int i = 1; i <= n; i++) if (!tin[i]) dfs(i, -1);
 }
