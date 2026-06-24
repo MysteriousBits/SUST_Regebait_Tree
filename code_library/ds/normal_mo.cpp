1bb7f7b469acae5cde52c0ba4742cc68a4cdb2b1
MysteriousBits <si.islam214@gmail.com>
2026-06-25 00:38:47 +0600

    Major changes for wildflower.mp3

--- code_library/ds/normal_mo.cpp
+++ code_library/ds/normal_mo.cpp
@@ -1,26 +1,31 @@
-void add(int ind, int end, int L, int R) { /* ... */ }
-void del(int ind, int end, int L, int R) { /* ... */ }  
-int calc(int L, int R) { /* ... */ }                     
-vector<int> mo(vector<pair<int, int>>& Q) {
-  int L = 1, R = 0, blk = 350;
-  vector<int> s(Q.size()), res(Q.size());
-  iota(s.begin(), s.end(), 0);
-  sort(s.begin(), s.end(), [&](int i, int j) {
-    int bi = Q[i].first / blk;
-    int bj = Q[j].first / blk;
-    if (bi != bj) return bi < bj;
-    if (bi & 1) return Q[i].second < Q[j].second;
-    return Q[i].second > Q[j].second;
-  });
-  for (int qi : s) {
-    pair<int, int> q = Q[qi];
-    int ql = q.first;
-    int qr = q.second;  
-    while (L > ql) { --L; add(L, 0, L, R); }
-    while (R < qr) { ++R; add(R, 1, L, R); }
-    while (L < ql) { del(L, 0, L, R); L++; }
-    while (R > qr) { del(R, 1, L, R); R--; }
-    res[qi] = calc(L, R);
-  }
-  return res;
-}
+struct MO{
+   int n,q,B,res; struct query{ int l,r,id; };
+   vector<query> Q; vector<int> ans,cnt;
+   MO(int n, int q): n(n), q(q){
+      res=0;
+      B=sqrt(n)+1;
+      ans.resize(q+1);
+      cnt.resize(n+10);
+   }
+   void add_query(int l, int r, int id){ Q.push_back({l,r,id});}
+   void add_left(int id,const vector<int>& a){}
+   void add_right(int id,const vector<int>& a){}
+   void rem_left(int id,const vector<int>& a){}
+   void rem_right(int id,const vector<int>& a){}
+   void get_ans(const vector<int>& a){
+      sort(Q.begin(),Q.end(),[&](auto& x, auto& y){
+         if(x.l/B == y.l/B){
+            return ((x.l/B)&1) ? x.r>y.r : x.r<y.r;
+         }
+         return x.l/B < y.l/B;
+      });
+      int L=1,R=0;
+      for(auto &[l,r,id]: Q){
+         while(L>l)  add_left(--L,a);
+         while(R<r)  add_right(++R,a);
+         while(L<l)  rem_left(L++,a);
+         while(R>r)  rem_right(R--,a);
+         ans[id]=res;
+      }
+   }
+};
--- code_library/string/SuffixArray.h
+++ code_library/string/SuffixArray.h
@@ -1,22 +1,26 @@
-struct SuffixArray {
-	vi sa, lcp;
-	SuffixArray(string s, int lim=256) { // or vector<int>
-		s.push_back(0); int n = sz(s), k = 0, a, b;
-		vi x(all(s)), y(n), ws(max(n, lim));
-		sa = lcp = y, iota(all(sa), 0);
-		for (int j = 0, p = 0; p < n; j = max(1, j * 2), lim = p) {
-			p = j, iota(all(y), n - j);
-			rep(i,0,n) if (sa[i] >= j) y[p++] = sa[i] - j;
-			fill(all(ws), 0);
-			rep(i,0,n) ws[x[i]]++;
-			rep(i,1,lim) ws[i] += ws[i - 1];
-			for (int i = n; i--;) sa[--ws[x[y[i]]]] = y[i];
-			swap(x, y), p = 1, x[sa[0]] = 0;
-			rep(i,1,n) a = sa[i - 1], b = sa[i], x[b] =
-				(y[a] == y[b] && y[a + j] == y[b + j]) ? p - 1 : p++;
-		}
-		for (int i = 0, j; i < n - 1; lcp[x[i++]] = k)
-			for (k && k--, j = sa[x[i] - 1];
-					s[i + k] == s[j + k]; k++);
-	}
-};
+void suffix_array(string s){
+   s+="$";
+   int32_t n=s.size(),N=max(n,int32_t(260));
+   vector<int32_t> sa(n),R(n);
+   for(int32_t i=0;i<n;i++)  sa[i]=i,  R[i]=s[i];
+   for(int32_t k=0;k<n;k?(k<<=1):k++){
+      vector<int32_t> nR(n),cnt(N);
+      auto nsa=sa;
+      for(int32_t i=0;i<n;i++) nsa[i]=(nsa[i]-k+n)%n,  cnt[R[i]]++;
+      for(int32_t i=1;i<N;i++) cnt[i]+=cnt[i-1];
+      for(int32_t i=n-1;i>=0;i--) sa[--cnt[R[nsa[i]]]]=nsa[i];
+      for(int32_t i=1,r=0;i<n;i++){
+         r+=(R[sa[i]]!=R[sa[i-1]] || R[(sa[i]+k)%n]!=R[(sa[i-1]+k)%n]);
+         nR[sa[i]]=r;
+      }  
+      swap(R,nR);
+   }
+   vector<int32_t> lcp(n-1);
+   for(int32_t i=0,k=0;i<n-1;i++){
+      int32_t pi=R[i];
+      int32_t j=sa[pi-1];
+      while(i+k<n && j+k<n && s[i+k]==s[j+k])   k++;
+      lcp[pi-1]=k;
+      if(k) k--;
+   }
+}
