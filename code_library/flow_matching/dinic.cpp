11d80b824d1dcbe0e9397c4befa71334635bf1f6
MD MONOWAR JAHAN SAIF <2022331043@student.sust.edu>
2026-06-25 15:04:41 +0600

    Refactor Dinic's algorithm for max flow
    Refactor Dinic's algorithm implementation to improve clarity and functionality. Introduced new edge structure and updated methods for adding edges and calculating max flow.

--- code_library/flow_matching/dinic.cpp
+++ code_library/flow_matching/dinic.cpp
@@ -1,42 +1,80 @@
+const int64_t inf = 1LL << 60; 
 struct Dinic {
-	struct Edge {
-		int to, rev;
-		ll c, oc;
-		ll flow() { return max(oc - c, 0LL); } // if you need flows
-	};
-	vi lvl, ptr, q;
-	vector<vector<Edge>> adj;
-	Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
-	void addEdge(int a, int b, ll c, ll rcap = 0) {
-		adj[a].push_back({b, sz(adj[b]), c, c});
-		adj[b].push_back({a, sz(adj[a]) - 1, rcap, rcap});
-	}
-	ll dfs(int v, int t, ll f) {
-		if (v == t || !f) return f;
-		for (int& i = ptr[v]; i < sz(adj[v]); i++) {
-			Edge& e = adj[v][i];
-			if (lvl[e.to] == lvl[v] + 1)
-				if (ll p = dfs(e.to, t, min(f, e.c))) {
-					e.c -= p, adj[e.to][e.rev].c += p;
-					return p;
-				}
-		}
-		return 0;
-	}
-	ll calc(int s, int t) {
-		ll flow = 0; q[0] = s;
-		rep(L,0,31) do { // 'int L=30' maybe faster for random data
-			lvl = ptr = vi(sz(q));
-			int qi = 0, qe = lvl[s] = 1;
-			while (qi < qe && !lvl[t]) {
-				int v = q[qi++];
-				for (Edge e : adj[v])
-					if (!lvl[e.to] && e.c >> (30 - L))
-						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
-			}
-			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
-		} while (lvl[t]);
-		return flow;
-	}
-	bool leftOfMinCut(int a) { return lvl[a] != 0; }
+   struct Edge {
+      int to, rev;
+      int64_t flow, w;
+      int id;
+   };
+   int n, s, t, mxid;
+   vector<int> d, done;
+   vector<int64_t> flow_through;
+   vector<vector<Edge>> g;
+   Dinic() {}
+   Dinic(int _n) {
+      n = _n + 10;
+      mxid = 0;
+      g.resize(n);
+   }
+   void add_edge(int u, int v, int64_t w, int id = -1) {
+      Edge a = {v, (int)g[v].size(), 0, w, id};
+      Edge b = {u, (int)g[u].size(), 0, 0, -2}; // cap(b) = w for bidirectional
+      g[u].emplace_back(a);
+      g[v].emplace_back(b);
+      if (id >= 0) mxid = max(mxid, id);
+   }
+   bool bfs() {
+      d.assign(n, -1);
+      d[s] = 0;
+      queue<int> q;
+      q.push(s);
+      while (!q.empty()) {
+         int u = q.front();
+         q.pop();
+         for (auto &e : g[u]) {
+            int v = e.to;
+            if (d[v] == -1 && e.flow < e.w) {
+               d[v] = d[u] + 1;
+               q.push(v);
+            }
+         }
+      }
+      return d[t] != -1;
+   }
+   int64_t dfs(int u, int64_t flow) {
+      if (u == t) return flow;
+      for (int &i = done[u]; i < (int)g[u].size(); i++) {
+         Edge &e = g[u][i];
+         if (e.w <= e.flow) continue;
+         int v = e.to;
+         if (d[v] == d[u] + 1) {
+            int64_t nw = dfs(v, min(flow, e.w - e.flow));
+            if (nw > 0) {
+               e.flow += nw;
+               g[v][e.rev].flow -= nw;
+               return nw;
+            }
+         }
+      }
+      return 0;
+   }
+   int64_t max_flow(int _s, int _t) {
+      s = _s;
+      t = _t;
+      int64_t flow = 0;
+      while (bfs()) {
+         done.assign(n, 0);
+         while (int64_t nw = dfs(s, inf)) {
+            flow += nw;
+         }
+      }
+      flow_through.assign(mxid+10,int64_t(0));
+      for (int i = 0; i < n; i++) {
+         for (auto &e : g[i]) {
+            if (e.id >= 0) {
+               flow_through[e.id] = e.flow;
+            }
+         }
+      }
+      return flow;
+   }
 };
