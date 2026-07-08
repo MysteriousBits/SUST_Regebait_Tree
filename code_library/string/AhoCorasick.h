68cb1a8723d7e3ff20c69414652ceea02c0b455b
MysteriousBits <si.islam214@gmail.com>
2026-07-08 23:51:57 +0600

    Replaced macros from AhoCorasick

--- code_library/string/AhoCorasick.h
+++ code_library/string/AhoCorasick.h
@@ -1,66 +1,66 @@
 struct AhoCorasick {
-	enum {alpha = 26, first = 'A'}; // change this!
-	struct Node {
-		// (nmatches is optional)
-		int back, next[alpha], start = -1, end = -1, nmatches = 0;
-		Node(int v) { memset(next, v, sizeof(next)); }
-	};
-	vector<Node> N;
-	vi backp;
-	void insert(string& s, int j) {
-		assert(!s.empty());
-		int n = 0;
-		for (char c : s) {
-			int& m = N[n].next[c - first];
-			if (m == -1) { n = m = sz(N); N.emplace_back(-1); }
-			else n = m;
-		}
-		if (N[n].end == -1) N[n].start = j;
-		backp.push_back(N[n].end);
-		N[n].end = j;
-		N[n].nmatches++;
-	}
-	AhoCorasick(vector<string>& pat) : N(1, -1) {
-		rep(i,0,sz(pat)) insert(pat[i], i);
-		N[0].back = sz(N);
-		N.emplace_back(0);
+  enum {alpha = 26, first = 'A'}; // change this!
+  struct Node {
+    // (nmatches is optional)
+    int back, next[alpha], start = -1, end = -1, nmatches = 0;
+    Node(int v) { memset(next, v, sizeof(next)); }
+  };
+  vector<Node> N;
+  vector<int> backp;
+  void insert(string& s, int j) {
+    assert(!s.empty());
+    int n = 0;
+    for (char c : s) {
+      int& m = N[n].next[c - first];
+      if (m == -1) { n = m = N.size(); N.emplace_back(-1); }
+      else n = m;
+    }
+    if (N[n].end == -1) N[n].start = j;
+    backp.push_back(N[n].end);
+    N[n].end = j;
+    N[n].nmatches++;
+  }
+  AhoCorasick(vector<string>& pat) : N(1, -1) {
+    for (int i = 0; i < (int)pat.size(); ++i) insert(pat[i], i);
+    N[0].back = N.size();
+    N.emplace_back(0);
 
-		queue<int> q;
-		for (q.push(0); !q.empty(); q.pop()) {
-			int n = q.front(), prev = N[n].back;
-			rep(i,0,alpha) {
-				int &ed = N[n].next[i], y = N[prev].next[i];
-				if (ed == -1) ed = y;
-				else {
-					N[ed].back = y;
-					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
-						= N[y].end;
-					N[ed].nmatches += N[y].nmatches;
-					q.push(ed);
-				}
-			}
-		}
-	}
-	vi find(string word) {
-		int n = 0;
-		vi res; // ll count = 0;
-		for (char c : word) {
-			n = N[n].next[c - first];
-			res.push_back(N[n].end);
-			// count += N[n].nmatches;
-		}
-		return res;
-	}
-	vector<vi> findAll(vector<string>& pat, string word) {
-		vi r = find(word);
-		vector<vi> res(sz(word));
-		rep(i,0,sz(word)) {
-			int ind = r[i];
-			while (ind != -1) {
-				res[i - sz(pat[ind]) + 1].push_back(ind);
-				ind = backp[ind];
-			}
-		}
-		return res;
-	}
+    queue<int> q;
+    for (q.push(0); !q.empty(); q.pop()) {
+      int n = q.front(), prev = N[n].back;
+      for (int i = 0; i < alpha; ++i) {
+        int &ed = N[n].next[i], y = N[prev].next[i];
+        if (ed == -1) ed = y;
+        else {
+          N[ed].back = y;
+          (N[ed].end == -1 ? N[ed].end : backp[N[ed].start]) = N[y].end;
+          N[ed].nmatches += N[y].nmatches;
+          q.push(ed);
+        }
+      }
+    }
+  }
+  vector<int> find(string word) {
+    int n = 0;
+    vector<int> res; // ll count = 0;
+    for (char c : word) {
+      n = N[n].next[c - first];
+      res.push_back(N[n].end);
+      // count += N[n].nmatches;
+    }
+    return res;
+  }
+  vector<vector<int>> findAll(vector<string>& pat, string word) {
+    vector<int> r = find(word);
+    int n = word.size();
+    vector<vector<int>> res(n);
+    for (int i = 0; i < n; ++i) {
+      int ind = r[i];
+      while (ind != -1) {
+        res[i - pat[ind].size() + 1].push_back(ind);
+        ind = backp[ind];
+      }
+    }
+    return res;
+  }
 };
