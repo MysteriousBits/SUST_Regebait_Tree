eb6520cb759fb365c79038f5ad0c93db8a82b7fe
MysteriousBits <si.islam214@gmail.com>
2026-07-08 23:37:13 +0600

    Replaced macros from kmp

--- code_library/string/KMP.h
+++ code_library/string/KMP.h
@@ -1,16 +1,17 @@
-vi pi(const string& s) {
-	vi p(sz(s));
-	rep(i,1,sz(s)) {
-		int g = p[i-1];
-		while (g && s[i] != s[g]) g = p[g-1];
-		p[i] = g + (s[i] == s[g]);
-	}
-	return p;
+vector<int> pi(const string& s) {
+  int n = s.size();
+  vector<int> p(n);
+  for (int i = 1; i < n; ++i) {
+    int g = p[i-1];
+    while (g && s[i] != s[g]) g = p[g-1];
+    p[i] = g + (s[i] == s[g]);
+  }
+  return p;
 }
-
-vi match(const string& s, const string& pat) {
-	vi p = pi(pat + '\0' + s), res;
-	rep(i,sz(p)-sz(s),sz(p))
-		if (p[i] == sz(pat)) res.push_back(i - 2 * sz(pat));
-	return res;
+vector<int> match(const string& s, const string& pat) {
+  vector<int> p = pi(pat + '\0' + s), res;
+  int n = p.size(), ns = s.size(), np = pat.size();
+  for (int i = n - ns; i < n; ++i)
+    if (p[i] == np) res.push_back(i - 2 * np);
+  return res;
 }
