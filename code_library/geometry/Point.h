20d1d6cced3e02ee2d2ef40e3640bb2e728181e9
MysteriousBits <si.islam214@gmail.com>
2026-07-08 23:55:42 +0600

    Fixed sgn() for floating point values

--- code_library/geometry/Point.h
+++ code_library/geometry/Point.h
@@ -1,6 +1,7 @@
 template <class T>
 int sgn(T x) {
   return (x > 0) - (x < 0);
+  // return (x > EPS) - (x < -EPS); // for float
 }
 template <class T>
 struct Point {
@@ -8,7 +9,7 @@ struct Point {
   T x, y;
   explicit Point(T x = 0, T y = 0) : x(x), y(y) {}
   bool operator<(P p) const { return tie(x, y) < tie(p.x, p.y); }
-  bool operator==(P p) const { return tie(x, y) == tie(p.x, p.y); }
+  bool operator==(P p) const { return tie(x, y) == tie(p.x, p.y); } // Use feq for float
   P operator+(P p) const { return P(x + p.x, y + p.y); }
   P operator-(P p) const { return P(x - p.x, y - p.y); }
   P operator*(T d) const { return P(x * d, y * d); }
