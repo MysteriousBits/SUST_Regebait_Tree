954c5bdb9cca3f704cdce7145936742bab98678e
MysteriousBits <si.islam214@gmail.com>
2026-07-18 13:26:05 +0600

    Added dist(p), angle(p), project, scalar_project and reflect

--- code_library/geometry/Point.h
+++ code_library/geometry/Point.h
@@ -18,9 +18,15 @@ struct Point {
   T cross(P p) const { return x * p.y - y * p.x; }
   T cross(P a, P b) const { return (a - *this).cross(b - *this); }
   T dist2() const { return x * x + y * y; }
+  T dist2(P p) const { return (*this - p).dist2(); }
   long double dist() const { return sqrt((long double)dist2()); }
+  long double dist(P p) const { return (*this - p).dist(); }
   // angle to x-axis in interval [-pi, pi]
   long double angle() const { return atan2(y, x); }
+  // Directed angle from vector 'this' to vector 'p' in interval [-pi, pi]
+  long double angle(P p) const {
+    return atan2((long double)this->cross(p), (long double)this->dot(p));
+  }
   P unit() const { return *this / dist(); }  // makes dist()=1
   P perp() const { return P(-y, x); }        // rotates +90 degrees
   P normal() const { return perp().unit(); }
@@ -28,6 +34,12 @@ struct Point {
   P rotate(long double a) const {
     return P(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
   }
+  // Project point 'p' onto the line formed by vector 'this' originating from origin
+  P project(P p) const { return *this * ((long double)this->dot(p) / dist2()); }
+  // Returns the signed length of the projection of vector 'p' onto vector 'this'
+  long double scalar_project(P p) const { return (long double)this->dot(p) / dist(); }
+  // Reflect point 'p' across the line formed by vector 'this' originating from origin
+  P reflect(P p) const { return project(p) * 2 - p; }
   friend ostream& operator<<(ostream& os, P p) {
     return os << "(" << p.x << "," << p.y << ")";
   }
