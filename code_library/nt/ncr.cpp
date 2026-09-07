be1ee86cc14bf28cb15e429533bc8a40ef185f0d
MysteriousBits <si.islam214@gmail.com>
2026-09-07 20:18:14 +0600

    O(1) nCr

--- code_library/nt/ncr.cpp
+++ code_library/nt/ncr.cpp
@@ -1,11 +1,11 @@
-ll fact[MAXN];
+ll fact[MAXN], finv[MAXN];
 void calc_fact(ll m = MOD) {
   fact[0] = 1;
-  for (int i = 1; i < MAXN; ++i) {
-    fact[i] = (fact[i - 1] * i) % m;
-  }
+  for (int i = 1; i < MAXN; ++i) fact[i] = (fact[i - 1] * i) % m;
+  finv[MAXN - 1] = inv(fact[MAXN - 1]);
+  for (int i = MAXN - 2; i >= 0; --i) finv[i] = mul(finv[i + 1], i + 1);
 }
 ll C(int n, int k, ll m = MOD) {
   if (k > n) return 0;
-  return dvd(fact[n], fact[k] * fact[n - k] % m, m);
+  return mul(fact[n], mul(finv[k], finv[n - k]));
 }
