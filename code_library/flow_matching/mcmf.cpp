b114c7858b2df81bf56811d8ab05e8a5c4f18006
MysteriousBits <si.islam214@gmail.com>
2026-09-22 23:30:20 +0600

    Updated mcmf

--- code_library/flow_matching/mcmf.cpp
+++ code_library/flow_matching/mcmf.cpp
@@ -1,75 +1,52 @@
-const ll INF = numeric_limits<ll>::max() / 4;
+#define INFL (ll) 1e18
 struct MCMF {
-  struct edge {
-    int from, to, rev;
-    ll cap, cost, flow;
-  };
-  int N;
-  vector<vector<edge>> ed;
-  vi seen;
-  vector<ll> dist, pi;
-  vector<edge*> par;
-  MCMF(int N) : N(N), ed(N), seen(N), dist(N), pi(N), par(N) {}
-  void addEdge(int from, int to, ll cap, ll cost) {
-    if (from == to) return;
-    ed[from].push_back(edge{from, to, sz(ed[to]), cap, cost, 0});
-    ed[to].push_back(edge{to, from, sz(ed[from]) - 1, 0, -cost, 0});
+  struct Edge { int u, v; ll oc, cap, cost; };
+  int s, t, n;
+  vector<vector<int>> g;
+  vector<Edge> edges;
+  vector<ll> dist, p; vector<int> par;
+
+  MCMF(int n, int s, int t) : s(s), t(t), n(n) { g.resize(n + 1); }
+
+  void add_edge(int u, int v, ll cap, ll cost) {
+    g[u].pb(edges.size()); g[v].pb(edges.size() + 1);
+    edges.pb({u, v, cap, cap, cost}); edges.pb({v, u, 0, 0, -cost});
   }
-  void path(int s) {
-    fill(all(seen), 0);
-    fill(all(dist), INF);
-    dist[s] = 0;
-    ll di;
-    __gnu_pbds::priority_queue<pair<ll, int>> q;
-    vector<decltype(q)::point_iterator> its(N);
-    q.push({0, s});
-    while (!q.empty()) {
-      s = q.top().second;
-      q.pop();
-      seen[s] = 1;
-      di = dist[s] + pi[s];
-      for (edge& e : ed[s])
-        if (!seen[e.to]) {
-          ll val = di - pi[e.to] + e.cost;
-          if (e.cap - e.flow > 0 && val < dist[e.to]) {
-            dist[e.to] = val;
-            par[e.to] = &e;
-            if (its[e.to] == q.end())
-              its[e.to] = q.push({-dist[e.to], e.to});
-            else
-              q.modify(its[e.to], {-dist[e.to], e.to});
-          }
-        }
-    }
-    rep(i, 0, N) pi[i] = min(pi[i] + dist[i], INF);
+  void setpot() {
+    p.assign(n + 1, INFL); p[s] = 0;
+    for (int i = 0; i < n; i++) for (auto& e : edges)
+      if (e.cap && p[e.u] != INFL) p[e.v] = min(p[e.v], p[e.u] + e.cost);
   }
-  pair<ll, ll> maxflow(int s, int t) {
-    ll totflow = 0, totcost = 0;
-    while (path(s), seen[t]) {
-      ll fl = INF;
-      for (edge* x = par[t]; x; x = par[x->from])
-        fl = min(fl, x->cap - x->flow);
-      totflow += fl;
-      for (edge* x = par[t]; x; x = par[x->from]) {
-        x->flow += fl;
-        ed[x->to][x->rev].flow -= fl;
+  bool dij() {
+    dist.assign(n + 1, INFL); dist[s] = 0;
+    vector<bool> done(n + 1); par.assign(n + 1, -1);
+    priority_queue<pair<ll,int>> pq; pq.push({0, s});
+    while (!pq.empty()) {
+      int u = pq.top().second; pq.pop();
+      if (done[u]) continue;
+      p[u] += dist[u]; done[u] = true;
+      for (int id : g[u]) {
+        auto& e = edges[id];
+        ll d = e.cost + p[u] - p[e.v];
+        if (!done[e.v] && e.cap && d < dist[e.v])
+          par[e.v] = id, dist[e.v] = d, pq.push({-d, e.v});
       }
     }
-    rep(i, 0, N) for (edge& e : ed[i]) totcost += e.cost * e.flow;
-    return {totflow, totcost / 2};
+    return dist[t] != INFL;
   }
-  // If some costs can be negative, call this before maxflow:
-  void setpi(int s) {  // (otherwise, leave this out)
-    fill(all(pi), INF);
-    pi[s] = 0;
-    int it = N, ch = 1;
-    ll v;
-    while (ch-- && it--)
-      rep(i, 0, N) if (pi[i] !=
-                       INF) for (edge& e :
-                                 ed[i]) if (e.cap) if ((v = pi[i] + e.cost) <
-                                                       pi[e.to]) pi[e.to] = v,
-                                                                 ch = 1;
-    assert(it >= 0);  // negative cost cycle
+  pair<ll,ll> min_cost_flow() {
+    setpot();
+    ll f = 0, mc = 0;
+    while (dij()) {
+      ll mn = INFL;
+      for (int u = t; u != s; u = edges[par[u]].u)
+        mn = min(mn, edges[par[u]].cap);
+      f += mn; mc += mn * p[t];
+      for (int u = t; u != s; u = edges[par[u]].u)
+        edges[par[u]].cap -= mn, edges[par[u] ^ 1].cap += mn;
+    }
+    return {f, mc};
   }
+  // flow on edge id (0-based)
+  ll flow(int id) { return edges[id << 1].oc - edges[id << 1].cap; }
 };
